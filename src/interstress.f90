41fd121ce1c20147ee9086da3291066418c3aed1
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-09-24 16:32:32 -0500

    refactor, rename

--- src/interstress.f90
+++ src/interstress.f90
@@ -1,13 +1,26 @@
-subroutine interstress(ic)
+subroutine interstress(cycleID)
 
     use globalvar
     implicit none
 
-    integer (kind=4) :: ic, ftPairIDtoNodeID(maxftnode*ntotft), ift, ntag, i, j, nuc, ndt
-    real (kind = dp) :: shearStress(totNumFtNode), normStress(totNumFtNode), ss0(totNumFtNode), ns0(totNumFtNode), &
-        strengthExcess(totNumFtNode) 
-    real (kind = dp) :: eta, etaMin, dtInSeconds, dtInYears, interseisElapsedTime, conYrToSec, rn, rs
-    real (kind = dp) :: theta, minstrengthexcess
+    integer (kind=4) :: cycleID
+    integer (kind=4) :: ftPairIDtoNodeID(maxftnode*ntotft)
+    integer (kind=4) :: ift, ntag, i, timeStepID
+    real (kind = dp) :: shearStress(totNumFtNode)
+    real (kind = dp) :: normStress(totNumFtNode)
+    real (kind = dp) :: initShear(totNumFtNode)
+    real (kind = dp) :: initNorm(totNumFtNode)
+    real (kind = dp) :: strengthExcess(totNumFtNode)
+    real (kind = dp) :: eta
+    real (kind = dp) :: etaMin
+    real (kind = dp) :: dtInSeconds
+    real (kind = dp) :: dtInYears
+    real (kind = dp) :: interseisElapsedTime
+    real (kind = dp) :: conYrToSec
+    real (kind = dp) :: rn
+    real (kind = dp) :: rs
+    real (kind = dp) :: theta
+    real (kind = dp) :: minStrengthExcess
     integer (kind = 4):: nuci(300), nucntag = 0
     character (len = 30) :: m1
     logical :: quit 
@@ -15,15 +28,15 @@ subroutine interstress(ic)
     write(m1,'(i6)') icstart
     m1=trim(adjustl(m1))
 
-    if (debug==1) write(*,*) 'INTERSTRESS: start. Cycle id is', ic
+    if (debug==1) write(*,*) 'INTERSTRESS: start. Cycle id is', cycleID
 
-    if (ic == 1) then 
-        ss0 = -ambientnorm * fric_fini
-        ns0 = ambientnorm
+    if (cycleID == 1) then 
+        initShear = -ambientnorm * fric_fini
+        initNorm = ambientnorm
     else
         do i = 1, totNumFtNode
-            ss0(i) = fistr(1,i) 
-            ns0(i) = fistr(2,i) 
+            initShear(i) = fistr(1,i) 
+            initNorm(i) = fistr(2,i) 
         enddo 
     endif
     ntag = 0  
@@ -46,12 +59,12 @@ subroutine interstress(ic)
     if (debug==1) write(*,*) conYrToSec
     dtInSeconds = dtInYears * conYrToSec
     interseisElapsedTime = 0.0d0
-    do ndt = 1, 100000
+    do timeStepID = 1, 100000
         if (quit .eqv. .TRUE.) then
             exit
         endif
         interseisElapsedTime = interseisElapsedTime + dtInSeconds
-        if (mod(ndt,100) == 1) then
+        if (mod(timeStepID,100) == 1) then
             write (*,*) 'Interseismic t = ', interseisElapsedTime/conYrToSec, ' years'
         endif
         do i = 1, totNumFtNode
@@ -73,8 +86,8 @@ subroutine interstress(ic)
             eta = nsmpGeoPhys(9,ftPairIDtoNodeID(i))     
             rs    = nsmpGeoPhys(6,ftPairIDtoNodeID(i)) *cos(2.0d0*theta)*eta ! eq.(3)
             rn    = -nsmpGeoPhys(6,ftPairIDtoNodeID(i)) *sin(2.0d0*theta)*eta ! eq.(3)
-            normStress(i) = (ns0(i) - ambientnorm - rn)*exp(-interseisElapsedTime*amu/eta)+rn+ambientnorm ! eq.(2) 
-            shearStress(i) = (ss0(i) - rs) * exp(-interseisElapsedTime*amu/eta) + rs ! eq.(1)
+            normStress(i) = (initNorm(i) - ambientnorm - rn)*exp(-interseisElapsedTime*amu/eta)+rn+ambientnorm ! eq.(2) 
+            shearStress(i) = (initShear(i) - rs) * exp(-interseisElapsedTime*amu/eta) + rs ! eq.(1)
             
             etaMin = -fric_fs*ambientnorm/maxShearStrainLoadRate
             if (eta<etaMin) then 
@@ -95,22 +108,11 @@ subroutine interstress(ic)
                             i==(nfnode(1)+nfnode(2)+nfnode(3))) then
                 strengthExcess(i) = 100.0d6
             endif
-            ! if (debug==1) then
-                ! write(*,*) 'rs, rn', rs, rn
-                ! write(*,*) 'ns(i), shearStress(i)', ns(i), shearStress(i)
-                ! write(*,*) eta, amu
-                ! write(*,*) strengthexcess(i)/1e6, abs(ns(i))/1e6, fric_fs, shearStress(i)/1e6
-            ! endif
         enddo
-        
-        !if (debug==1) write(*,*) 'INTERSTRESS: finish assigning straining rate.'
-        
+
         minStrengthExcess = minval(strengthExcess)
         loc = minloc(strengthExcess, dim = 1)
-        
-        !if (debug==1) write(*,*) 'INTERSTRESS: strength excess is', strengthexcess
-        !if (debug==1) write(*,*) 'INTERSTRESS: loc is', loc, 'min strength is', minstrengthexcess
-        
+  
         nucntag = 0
         do i = 1, totNumFtNode
             if ((i >1.and.i<nfnode(1)).or.(i>(1+nfnode(1)).and. &
